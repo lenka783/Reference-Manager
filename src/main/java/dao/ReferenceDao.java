b737ae0d2f2f7fdfb31dcf4260cc3ea670ea7174
Jan Bílek <honza@elisiondesign.cz>
2017-10-28 22:08:32 +0200

    closes #6 - create Reference DAO

--- src/main/java/dao/ReferenceDao.java
+++ src/main/java/dao/ReferenceDao.java
@@ -1,4 +1,17 @@
 package dao;
 
+import entity.Reference;
+
+import java.util.List;
+
 public interface ReferenceDao {
+    void create(Reference r);
+
+    void update(Reference r);
+
+    void remove(Reference r);
+
+    Reference findById(Long id);
+
+    List<Reference> findAll();
 }
