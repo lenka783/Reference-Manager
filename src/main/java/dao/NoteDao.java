2b8a671abfc7f0d974d0bb81bb76bbb713351354
Lenka Smitalova <Lenka@Lenkas-MacBook-Pro.local>
2017-10-29 15:27:09 +0100

    =NodeDao updated + NoteDaoImpl created

--- src/main/java/dao/NoteDao.java
+++ src/main/java/dao/NoteDao.java
@@ -1,4 +1,18 @@
 package dao;
 
+import entity.Note;
+
+import java.util.List;
+
 public interface NoteDao {
+
+    void create(Note note);
+
+    void update(Note note);
+
+    void remove(Note note);
+
+    Note findById(Long id);
+
+    List<Note> findAll();
 }
---
+++ src/main/java/dao/NoteDaoImpl.java
@@ -0,0 +1,41 @@
+package dao;
+
+import entity.Note;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+/**
+ * @author Lenka Šmitalová
+ */
+public class NoteDaoImpl implements NoteDao {
+    @PersistenceContext
+    EntityManager em;
+
+    @Override
+    public void create(Note note) {
+        em.persist(note);
+    }x
+
+    @Override
+    public void update(Note note) {
+        em.merge(note);
+    }
+
+    @Override
+    public void remove(Note note) {
+        Note managed = em.merge(note);
+        em.detach(managed);
+    }
+
+    @Override
+    public Note findById(Long id) {
+        return em.find(Note.class, id);
+    }
+
+    @Override
+    public List<Note> findAll() {
+        return em.createQuery("select n from Note n", Note.class).getResultList();
+    }
+}
