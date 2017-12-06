ac8d147d1f0b53f95f7cbbe48f8cef5d7ecdd215
David Šarman <mail@david-sarman.com>
2017-12-06 18:22:00 +0100

    Fixed ui maven to run npm according to active profile

--- referenceManager-react/src/store/configureStore.js
+++ referenceManager-react/src/store/configureStore.js
@@ -1,9 +1,5 @@
-// TODO: fix this, make default profile development instead of production
-/*
 if (process.env.NODE_ENV === 'production') {
     module.exports = require('./configureStore.prod'); // eslint-disable-line global-require
 } else {
     module.exports = require('./configureStore.dev'); // eslint-disable-line global-require
-}*/
-
-module.exports = require('./configureStore.dev'); // eslint-disable-line global-require
+}
