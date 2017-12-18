dea28dede413f862a5a03b48a4e8c2645a6ef7e8
David Šarman <mail@david-sarman.com>
2017-12-18 22:18:55 +0100

    Added redux-api

---
+++ web/src/main/react/src/rest.js
@@ -0,0 +1,32 @@
+import "isomorphic-fetch";
+import reduxApi, {transformers} from "redux-api";
+import adapterFetch from "redux-api/lib/adapters/fetch";
+
+const rest = reduxApi({
+    tags: {
+        url: `tags/`,
+        transformer: transformers.array,
+        // base endpoint options `fetch(url, options)`
+        options: {
+            headers: {
+                "Accept": "application/json",
+            }
+        }
+    },
+    tag: {
+        url: `tags/:id`,
+        crud: true
+    }
+});
+rest.use("fetch", adapterFetch(fetch)); // it's necessary to point using REST backend
+rest.use("rootUrl", "http://localhost:8080/pa165/rest/");
+rest.use("responseHandler",
+    (err, data)=> {
+        if (err) {
+            console.log(err);
+        } else {
+            return data;
+        }
+});
+
+export default rest;
--- web/src/main/react/src/store/configureStore.dev.js
+++ web/src/main/react/src/store/configureStore.dev.js
@@ -1,16 +1,19 @@
 import { applyMiddleware, combineReducers, createStore } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import { reducer as formReducer } from 'redux-form';
+import thunk from 'redux-thunk';
+import rest from '../rest';
 
 const middlewareWithDevTools = composeWithDevTools(
     applyMiddleware()
 );
 
-const store = createStore(
-    combineReducers({
-        form: formReducer
-    }),
-    middlewareWithDevTools
-);
+const createStoreWithMiddleware = applyMiddleware(thunk)(createStore);
+const reducer = combineReducers({
+    form: formReducer,
+    ...rest.reducers
+});
+
+const store = createStoreWithMiddleware(reducer, middlewareWithDevTools);
 
 export { store };
