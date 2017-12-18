ff17e53943bfa782ec6ea3181c04ead82796ae00
Jan Bílek <honza@elisiondesign.cz>
2017-12-18 12:41:19 +0100

    prepare tags view

--- referenceManager-react/src/views/ReferencesView/index.js
+++ referenceManager-react/src/views/ReferencesView/index.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {Card} from 'semantic-ui-react'
+import {Card} from 'semantic-ui-react';
 import faker from 'faker';
 import _ from 'lodash';
 import ReferenceCard from "../../components/ReferenceCard";
